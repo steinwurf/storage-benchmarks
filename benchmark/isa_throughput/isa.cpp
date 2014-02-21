22a665f7a074f4c75cc9feda4846a0aa6e360607
Peter Vingelmann <petya@master.com>
2014-02-21 03:00:18 +0100

    Integrated encoding benchmark for ISA

---
+++ benchmark/isa_throughput/isa.cpp
@@ -0,0 +1,230 @@
+// Copyright Steinwurf ApS 2011-2012.
+// Distributed under the "STEINWURF RESEARCH LICENSE 1.0".
+// See accompanying file LICENSE.rst or
+// http://www.steinwurf.com/licensing
+
+#include <ctime>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>  // for memset, memcmp
+
+#include <vector>
+
+#include <gauge/gauge.hpp>
+
+extern "C"
+{
+#include "erasure_code.h"
+#include "test.h"
+}
+
+#include "../throughput_benchmark.hpp"
+
+#define TEST_SOURCES 250
+#define MMAX TEST_SOURCES
+#define KMAX TEST_SOURCES
+
+typedef unsigned char u8;
+
+struct isa_encoder
+{
+    isa_encoder(uint32_t symbols, uint32_t symbol_size) :
+        m_symbols(symbols), m_symbol_size(symbol_size)
+    {
+        k = m_symbols;
+        m = 2 * m_symbols;
+        m_block_size = m_symbols * m_symbol_size;
+        m_payload_count = m_symbols;
+
+        // Symbol size must be a multiple of 64
+        assert(m_symbol_size % 64 == 0);
+
+        // Allocate the arrays
+        int i, j;
+        void* buf = 0;
+        for (i = 0; i < m; i++)
+        {
+            if (posix_memalign(&buf, 64, m_symbol_size))
+            {
+                printf("alloc error: Fail\n");
+            }
+            m_buffs[i] = (uint8_t*)buf;
+        }
+
+        // Make random data
+        for(i = 0; i < k; i++)
+            for(j = 0; j < (int)m_symbol_size; j++)
+                m_buffs[i][j] = rand();
+    }
+
+    ~isa_encoder()
+    {
+        for (int i = 0; i < m; i++)
+        {
+            free(m_buffs[i]);
+        }
+    }
+
+    void encode_all()
+    {
+        assert(m_payload_count == (uint32_t)(m-k));
+
+        gf_gen_rs_matrix(a, m, k);
+
+        // Make parity vects
+        ec_init_tables(k, m - k, &a[k * k], g_tbls);
+        ec_encode_data_sse(m_symbol_size,
+            k, m - k, g_tbls, m_buffs, &m_buffs[k]);
+    }
+
+    uint32_t block_size() { return m_block_size; }
+    uint32_t symbol_size() { return m_symbol_size; }
+    uint32_t payload_size() { return m_symbol_size; }
+    uint32_t payload_count() { return m_payload_count; }
+
+protected:
+
+    friend class isa_decoder;
+
+    uint8_t* m_buffs[TEST_SOURCES];
+    uint8_t a[MMAX*KMAX];
+    uint8_t g_tbls[KMAX*TEST_SOURCES*32];
+
+    // Code parameters
+    int k, m;
+
+    // Number of symbols
+    uint32_t m_symbols;
+    // Size of k+m symbols
+    uint32_t m_symbol_size;
+    // Size of a full generation (k symbols)
+    uint32_t m_block_size;
+    // Number of generated payloads
+    uint32_t m_payload_count;
+};
+
+
+struct isa_decoder
+{
+    isa_decoder(uint32_t symbols, uint32_t symbol_size) :
+        m_symbols(symbols), m_symbol_size(symbol_size)
+    {
+        k = m_symbols;
+        m = 2 * m_symbols;
+
+        m_block_size = m_symbols * m_symbol_size;
+        m_decoding_result = -1;
+    }
+
+    ~isa_decoder()
+    {
+    }
+
+    void decode_all(std::shared_ptr<isa_encoder> encoder)
+    {
+        uint32_t payload_count = encoder->payload_count();
+
+        assert(payload_count == (uint32_t)(m - k));
+    }
+
+    bool verify_data(std::shared_ptr<isa_encoder> encoder)
+    {
+        //assert(m_data_out.size() == encoder->m_data_in.size());
+        //return std::equal(m_data_out.begin(), m_data_out.end(),
+        //                 encoder->m_data_in.begin());
+
+        return true;
+    }
+
+    bool is_complete() { return (m_decoding_result != -1); }
+
+    uint32_t block_size() { return m_block_size; }
+    uint32_t symbol_size() { return m_symbol_size; }
+    uint32_t payload_size() { return m_symbol_size; }
+
+protected:
+
+    uint8_t* m_buffs[TEST_SOURCES];
+    uint8_t a[MMAX*KMAX], b[MMAX*KMAX], c[MMAX*KMAX], d[MMAX*KMAX];
+    uint8_t g_tbls[KMAX*TEST_SOURCES*32], src_in_err[TEST_SOURCES];
+    uint8_t src_err_list[TEST_SOURCES], *recov[TEST_SOURCES];
+
+    // Code parameters
+    int k, m;
+
+    // Number of symbols
+    uint32_t m_symbols;
+    // Size of k+m symbols
+    uint32_t m_symbol_size;
+    // Size of a full generation (k symbols)
+    uint32_t m_block_size;
+
+    int m_decoding_result;
+};
+
+BENCHMARK_OPTION(throughput_options)
+{
+    gauge::po::options_description options;
+
+    std::vector<uint32_t> symbols;
+    symbols.push_back(16);
+//     symbols.push_back(32);
+//     symbols.push_back(64);
+//     symbols.push_back(128);
+//     symbols.push_back(256);
+//     symbols.push_back(512);
+
+    auto default_symbols =
+        gauge::po::value<std::vector<uint32_t> >()->default_value(
+            symbols, "")->multitoken();
+
+    // Symbol size must be a multiple of 32
+    std::vector<uint32_t> symbol_size;
+    symbol_size.push_back(1000000);
+
+    auto default_symbol_size =
+        gauge::po::value<std::vector<uint32_t> >()->default_value(
+            symbol_size, "")->multitoken();
+
+    std::vector<std::string> types;
+    types.push_back("encoder");
+    //types.push_back("decoder");
+
+    auto default_types =
+        gauge::po::value<std::vector<std::string> >()->default_value(
+            types, "")->multitoken();
+
+    options.add_options()
+        ("symbols", default_symbols, "Set the number of symbols");
+
+    options.add_options()
+        ("symbol_size", default_symbol_size, "Set the symbol size in bytes");
+
+    options.add_options()
+        ("type", default_types, "Set type [encoder|decoder]");
+
+    gauge::runner::instance().register_options(options);
+}
+
+//------------------------------------------------------------------
+// Reed-Solomon Vandermonde
+//------------------------------------------------------------------
+
+typedef throughput_benchmark<isa_encoder, isa_decoder>
+    isa_throughput;
+
+BENCHMARK_F(isa_throughput, ISA, ErasureCode, 1)
+{
+    run_benchmark();
+}
+
+int main(int argc, const char* argv[])
+{
+    srand(static_cast<uint32_t>(time(0)));
+
+    gauge::runner::add_default_printers();
+    gauge::runner::run_benchmarks(argc, argv);
+
+    return 0;
+}
