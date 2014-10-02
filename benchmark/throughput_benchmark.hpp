a7fae1c3a7f1fa4c539023d201630899ee0be19c
Peter Vingelmann <petya@master.com>
2014-10-02 02:50:22 +0200

    Remove unused code

--- benchmark/throughput_benchmark.hpp
+++ benchmark/throughput_benchmark.hpp
@@ -11,7 +11,7 @@
 
 #include <gauge/gauge.hpp>
 
-/// A test block represents an encoder and decoder pair
+
 template<class Encoder, class Decoder>
 struct throughput_benchmark : public gauge::time_benchmark
 {
@@ -38,8 +38,6 @@ struct throughput_benchmark : public gauge::time_benchmark
     {
         // Get the time spent per iteration
         double time = gauge::time_benchmark::measurement();
-
-        //printf("Coding time: %.3f us\n", time);
 
         gauge::config_set cs = get_current_configuration();
         std::string type = cs.get_value<std::string>("type");
@@ -114,7 +112,6 @@ struct throughput_benchmark : public gauge::time_benchmark
 
         // Force a single iteration (repeated tests produce unstable results)
         return true;
-        //return gauge::time_benchmark::accept_measurement();
     }
 
     std::string unit_text() const
